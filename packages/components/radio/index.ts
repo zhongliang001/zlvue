01932dffd3db91ab58826c7b592111057915420c
Liang zhong <coolzhongliang@126.com>
2023-11-12 18:06:10 +0800

    feat(radio.vue): 新增单选功能

---
+++ packages/components/radio/index.ts
@@ -0,0 +1,6 @@
+import withInstall from '@zl-vue/utils/withInstall'
+import Radio from './src/radio.vue'
+
+const ZlRadio = withInstall(Radio)
+export default ZlRadio
+export * from './src/radio'
---
+++ packages/components/radio/src/radio.vue
@@ -0,0 +1,24 @@
+<template>
+  <template v-for="key in radioData?.keys()" :key="key">
+    <input type="radio" name="name" :value="key" @click="sel(radioData?.get(key))" />
+    {{ key }}
+  </template>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { radioProps } from './radio'
+
+defineOptions({
+  name: 'ZlRadio'
+})
+defineProps(radioProps)
+const selData = ref<string>()
+const emit = defineEmits(['update:modelValue'])
+const sel = (data: string | undefined) => {
+  if (!data) return
+  if (selData.value) {
+    selData.value = data
+  }
+  emit('update:modelValue', data)
+}
+</script>
